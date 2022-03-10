3676a113c0e9f826fe413731f5e3608229c66550
Juliano Grobério <60751281+fyrhen@users.noreply.github.com>
2022-03-10 16:16:38 +0000

    Initial commit

---
+++ routes/app.js
@@ -0,0 +1,21 @@
+var express = require('express'); 
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+
+router.get('/message', function (req, res, next) {
+    res.render('node', {message: 'Olá nova rota de mensagem.'});
+});
+
+router.post('/message', function (req, res, next) {
+    var messageVar = req.body.messageBody;
+    res.redirect('/message/' + messageVar);
+});
+
+router.get('/message/:msgParam', function (req, res, next) {
+    res.render('node', {message: req.params.msgParam});
+});
+
+module.exports = router;
