68694b7f2612fe13295fd804a7c39011968ca2f0
Juliano Grobério <julianogroberio@gmail.com>
2022-03-31 17:09:55 -0300

    parte IV - a

--- routes/app.js
+++ routes/app.js
@@ -19,5 +19,35 @@ router.get('/message/:msgParam', function (req, res, next) {
 });
 
 var User = require('../models/user');
+router.get('/node-mongodb-mongoose-user', function (req, res, next) {
+    res.render('node');    
+});
+
+router.post('/node-mongodb-mongoose-user', function (req, res, next) {
+    var emailVar = req.body.emailBody;
+    var userObject = new User({
+        firstName: 'Vinicius',
+        lastName: 'Rosalen',
+        password: "Segredo",
+        email: emailVar
+    });  
+    userObject.save();
+    
+    res.redirect('/node-mongodb-mongoose-user');
+});
+
+router.get('/node-mongodb-mongoose-user-busca', function (req, res, next) {
+    User.findOne({}, function(err, documents) {
+        if (err) {
+            return res.send('Error!!');
+        }
+        res.render('node', {firstNameV: documents.firstName,
+                            lastNameV: documents.lastName,
+                            passwordV: documents.password,
+                            emailV: documents.email,
+                            messagesV: documents.message
+        });
+    });
+});
 
 module.exports = router;
